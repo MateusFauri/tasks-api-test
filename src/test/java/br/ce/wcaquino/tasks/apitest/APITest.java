a8297fd10e1a13e7f9869de0d5d68d86766fd52f
Mateus Fauri <faurimateus@gmail.com>
2022-05-20 01:59:05 -0300

    teste de remoção

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -1,8 +1,13 @@
 package br.ce.wcaquino.tasks.apitest;
 
+import java.net.MalformedURLException;
+
 import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -47,6 +52,29 @@ public class APITest {
 			.statusCode(400)
 			.body("message", CoreMatchers.is("Due date must not be in past"))
 		;
+	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+		//inserir tarefa
+		Integer id = RestAssured.given()
+		.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2050-01-30\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201)
+			.extract().path("id")
+		;
+			
+		//remover tarefa
+		RestAssured.given()
+		.when()
+			.delete("/todo/"+id)
+		.then()
+			.statusCode(204)
+		;
+
 	}
 }
 
